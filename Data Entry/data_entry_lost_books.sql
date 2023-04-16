03f6305a028ebe3e597ff158c2756135fc7d0b14
musfikurrahman24 <smmusfikurrahman1924@gmail.com>
2023-04-17 01:34:43 +0600

    data entry

---
+++ Data Entry/data_entry_lost_books.sql
@@ -0,0 +1,3 @@
+INSERT INTO `lost_books` (`lost_id`, `borrow_id`, `lost_date`, `fine`) VALUES
+(1004, 100204, '2023-03-11', 15),
+(1006, 100206, '2023-03-13', 20);
---
+++ Data Entry/data_entry_return_book.sql
@@ -0,0 +1,4 @@
+INSERT INTO `return_books` (`return_id`, `borrow_id`, `return_date`, `fine`) VALUES
+(1, 100201, '2023-03-08', 0),
+(2, 100202, '2023-03-09', 0),
+(3, 100205, '2023-03-10', 0);
